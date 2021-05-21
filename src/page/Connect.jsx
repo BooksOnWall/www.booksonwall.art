d9ed9cf36e8d5f06b2159e0878b070d504ed0804
Tom Bouillut <tom@booksonwall.art>
2021-05-21 16:12:48 -0300

    changed class to function

--- src/page/Connect.jsx
+++ src/page/Connect.jsx
@@ -3,7 +3,8 @@ import {
     Box,
     Grid,
     Container,
-    Typography
+    Typography,
+
   } from '@material-ui/core';
 
 import { injectIntl, defineMessages } from 'react-intl';
@@ -13,6 +14,7 @@ import Register from './Register'
 import Social from './Social';
 import projects from '../md/projects/index';
 import Projects from './projects/Projects';
+
 const connectTraductions = defineMessages({
 keed_in_touch:{
   id: 'connect.keed_in_touch',
@@ -24,59 +26,49 @@ register:{
 }
 });
 
-class Connect extends Component {
-  constructor(props) {
-    super(props)
+const Connect = (props) => {
 
-    this.state = {  }
-  }
-  componentDidMount() {
-    // update authenticated state on logout
-
-  }
-  goTo = (e, b) => window.location.href = b.src
-  render() {
-    const { locale, messages } = this.props.intl;
-
-    return (
-      <Box className="connect">
-        <Box className="contact">
-        <Container maxWidth="xl">
-        <Grid container spacing={10} >
-          <Grid item xs={12} md={4} xl={4} className="contactGrid">
-              <Typography color="textSecondary" variant='h2'> {messages.connect.keed_in_touch}</Typography>
-          </Grid>
-          <Grid item xs={12} md={5} xl={5} className="contactGrid">
-              <ContactForm messages={messages} locale={locale}/>
-          </Grid>
-          <Grid item xs={12} md={3} xl={3}>
-            <Social tab goTo={this.goTo}/>
-          </Grid>
+  const goTo = (e, b) => window.location.href = b.src;
+  const { locale, messages } = props.intl;
+  return (
+    <Box className="connect">
+      <Box className="contact">
+      <Container maxWidth="xl">
+      <Grid container spacing={10} >
+        <Grid item xs={12} md={4} xl={4} className="contactGrid">
+            <Typography color="textSecondary" variant='h2'> {messages.connect.keed_in_touch}</Typography>
         </Grid>
-        </Container>
-        </Box>
-
-        <Box className="register">
-          <Container maxWidth="xl">
-            <Grid container spacing={10} >
-              <Grid item xs={12} md={6} xl={4} >
-                <Typography color="textSecondary" variant='h2'> {messages.connect.register}</Typography>
-              </Grid>
-              <Grid item xs={12} md={6} xl={4} >
-                <Register messages={messages}/>
-              </Grid>
-            </Grid>
-          </ Container>
-        </Box>
-
-        <Box className="projects">
-        <Container maxWidth="xl">
-          <Projects messages={messages} locale={locale} projects={projects}/>
-          </ Container>
-        </Box>
+        <Grid item xs={12} md={5} xl={5} className="contactGrid">
+            <ContactForm messages={messages} locale={locale}/>
+        </Grid>
+        <Grid item xs={12} md={3} xl={3}>
+          <Social tab goTo={goTo}/>
+        </Grid>
+      </Grid>
+      </Container>
       </Box>
-    )
-  }
-};
+
+      <Box className="register">
+        <Container maxWidth="xl">
+          <Grid container spacing={10} >
+            <Grid item xs={12} md={6} xl={4} >
+              <Typography color="textSecondary" variant='h2'> {messages.connect.register}</Typography>
+            </Grid>
+            <Grid item xs={12} md={6} xl={4} >
+              <Register messages={messages}/>
+            </Grid>
+          </Grid>
+        </ Container>
+      </Box>
+
+      <Box className="projects">
+      <Container maxWidth="xl">
+        <Projects messages={messages} locale={locale} projects={projects}/>
+        </ Container>
+      </Box>
+    </Box>
+  );
+}
+
 
 export default injectIntl(Connect);
