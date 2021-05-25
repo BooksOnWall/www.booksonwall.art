6888fc51219c24d0838d1a72293539a42cbeec7a
Tom Bouillut <tom@booksonwall.art>
2021-05-25 14:47:05 -0300

    added react hook form to contact page

--- src/page/ContactForm.jsx
+++ src/page/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import {
   Button,
   TextField,
@@ -6,7 +6,7 @@ import {
   Box,
   Typography
   } from '@material-ui/core';
-
+import { useForm, Controller } from "react-hook-form";
 import { defineMessages } from 'react-intl';
 
 const contactTraductions = defineMessages({
@@ -32,121 +32,152 @@ const contactTraductions = defineMessages({
   }
 });
 
-export default class ContactForm extends Component {
-  constructor(props) {
-    super(props)
+const ContactForm = ({messages, locale}) => {
 
-    this.state = {
-      trad: contactTraductions,
-      initialState: {
-        first_name: '',
-        name: '',
-        email: '',
-        company: '',
-        message:  '',
-        phone: '',
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+
+    const { register, control, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+    console.log(watch("name")); // watch input value by passing the name of it
+    const options = [
+      {
+        key: 'general',
+        text: 'General enquiry',
+        value: 'general',
+
       },
-    }
-  }
-  componentDidMount() {
-    // update authenticated state on logout
+      {
+        key: 'new story',
+        text: 'Proposal for a new story',
+        value: 'new story',
 
-  }
-  postForm = () => {}
-  render() {
-    const {initialState} = this.state;
-    const{messages} = this.props;
-    // const options = [
-    //   {
-    //     key: 'general',
-    //     text: 'General enquiry',
-    //     value: 'general',
-    //
-    //   },
-    //   {
-    //     key: 'new story',
-    //     text: 'Proposal for a new story',
-    //     value: 'new story',
-    //
-    //   },
-    //   {
-    //     key: 'new partner',
-    //     text: 'I want to become a partner',
-    //     value: 'new partner',
-    //
-    //   },
-    //   {
-    //     key: 'join team',
-    //     text: 'I want to be part of the team',
-    //     value: 'new partner',
-    //
-    //   },
-    //   {
-    //     key: 'press',
-    //     text: 'Press',
-    //     value: 'press',
-    //
-    //   },
-    // ];
+      },
+      {
+        key: 'new partner',
+        text: 'I want to become a partner',
+        value: 'new partner',
+
+      },
+      {
+        key: 'join team',
+        text: 'I want to be part of the team',
+        value: 'new partner',
+
+      },
+      {
+        key: 'press',
+        text: 'Press',
+        value: 'press',
+
+      },
+    ];
     return (
       <Box id="contactForm" >
       <Typography gutterBottom color="textSecondary" variant="h4">{messages.contact.contactUs}</Typography>
-          <form  onSubmit={this.handleSubmit}>
+      <form  onSubmit={handleSubmit(onSubmit)}>
+
           {/*<Dropdown
                 placeholder='How can we help you ?'
                 fluid
                 selection
                 options={options}
               /> */}
+              <Controller
+                name="name"
+                control={control}
+                value={name}
+                rules={{ required: 'name required' }}
+                render={({ field: { onChange, value }, fieldState: { error }  }) => (
+                  <TextField
+                    fullWidth
+                    className="formImput"
+                    label={messages.contact.name}
+                    placeholder={messages.contact.name}
+                    autoFocus={true}
+                    type="text"
+                    variant="filled"
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    value={value}
+                    onChange={onChange}
+                  />
+                )}
+              />
 
-            <TextField
-              className="formImput"
-              fullWidth
-              label={messages.contact.name}
-              placeholder={messages.contact.name}
-              autoFocus={true}
-              type="text"
-              name="name"
-              onChange={this.handleChange}
-              defaultValue={initialState.name}
-              />
               <br /><br />
-            <TextField
-              className="formImput"
-              fullWidth
-              label={messages.contact.email}
-              placeholder={messages.contact.email}
-              type="email"
-              name="email"
-              onChange={this.handleChange}
-              defaultValue={initialState.email}
+              <Controller
+                name="email"
+                control={control}
+                defaultValue=""
+                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                  <TextField
+                    fullWidth
+                    className="formImput"
+                    label={messages.contact.email}
+                    placeholder={messages.contact.email}
+                    variant="filled"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    type="email"
+                  />
+                )}
+                rules={{ required: 'Email required' }}
               />
+
               <br /><br />
-              <TextField
-                className="formImput"
-                fullWidth
-                label={messages.contact.phone}
-                placeholder={messages.contact.phone}
-                type="text"
+              <Controller
                 name="phone"
-                onChange={this.handleChange}
-                defaultValue={initialState.phone}
-                />
+                control={control}
+                defaultValue=""
+                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                  <TextField
+                    fullWidth
+                    className="formImput"
+                    label={messages.contact.phone}
+                    placeholder={messages.contact.phone}
+                    variant="filled"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    type="text"
+                  />
+                )}
+                rules={{ required: 'Phone required' }}
+              />
               <br /><br />
-            <TextareaAutosize
-              fullWidth
-              rowsMin={10}
-              rows={10}
-              className="formMessage"
-              name="message"
-              placeholder='Message'
-              onChange={this.handleChange}
-              defaultValue={initialState.message}
-            />
+              <Controller
+                name="message"
+                control={control}
+                defaultValue=""
+                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                  <TextareaAutosize
+                    fullWidth
+                    rowsMin={10}
+                    rows={10}
+                    className="formMessage"
+                    name="message"
+                    label={messages.contact.message}
+                    placeholder={messages.contact.message}
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                  />
+                )}
+                rules={{ required: 'Message required' }}
+              />
+
             <br /><br />
-            <Button className="button2" disableElevation label={messages.contact.send}>{messages.contact.send}</Button>
+            <Button type="submit" className="button2" disableElevation label={messages.contact.send}>{messages.contact.send}</Button>
           </form>
       </Box>
     );
-  }
 }
+export default ContactForm
