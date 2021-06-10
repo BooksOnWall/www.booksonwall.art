ec10bd52ab12d15cbbb9710b5c9642b93f03e709
Tom Bouillut <tom@booksonwall.art>
2021-06-10 12:10:21 -0300

    debug extract:messages

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,16 @@ module.exports = {
         test: /\bmapbox-gl-csp-worker.js\b/i,
         use: { loader: "worker-loader" },
       },
+      {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        }
+      }
+    }
     ],
   },
 };
