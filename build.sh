bb6d9098a0ec40be2b8598f2eb07c9e3878495ba
Tom Bouillut <tom@croll.fr>
2022-12-16 12:42:57 -0300

    second commit

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm use 14
-echo "##   updtae browser list database"
+echo "##   update browser list database"
 npx browserslist@latest --update-db
+npm i -g yarn
 echo "##   git pull disabled"
 # git pull 
 echo "##   install packages ..."
@@ -14,6 +15,6 @@ echo "##   build start ..."
 yarn build
 echo "##   build complete ..."
 echo "##   set permissions ..."
-chown -R www-data:www-data public
+chown -R booksonwall:booksonwall public
 rm -rf node_modules
 echo "##   node_modules deleted !"
